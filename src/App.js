24a5e085f9dfb2196c03c807a752a418732c17ec
Abhinandan Mishra <abhinandanmishra360@gmail.com>
2022-02-23 01:44:46 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
-import React from 'react';
-import Card from './Cards';
-import Data from './Data';
-import './App.css';
+import React from "react";
+import Card from "./Cards";
+import Data from "./Data";
+import "./App.css";
 
-function eachCard(val){
-    return ( 
-   <Card imgsrc={val.imgsrc} title={val.title} link={val.link} description={val.description} />
-    )
-        
+function eachCard(val) {
+	return (
+		<Card
+			imgsrc={val.imgsrc}
+			title={val.title}
+			link={val.link}
+			description={val.description}
+		/>
+	);
 }
-function App(){
-   return(
-       <>
-       <h1 className="neons">Favourite Web-Series Collections</h1>
+function App() {
+	return (
+		<>
+			<h1 className="neons">Favourite Web-Series Collections</h1>
 
-        {Data.map(eachCard)}
-
-       <div className="footer">
-        <h4>Created by-</h4>
-        <span className="created-by"><strong><a href="https://www.linkedin.com/in/abhinandanmishra1">Abhinandan Mishra</a></strong></span>
-       </div>
-       </>
-   )
+			<div className="container">{Data.map(eachCard)}</div>
+		</>
+	);
 }
 
 export default App;
--- src/Cards.js
+++ src/Cards.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import './Cards.css';
-function Card(props){
-   return(<>
-   
-    <div className="cards">
-        <div className="card-img">
-            <img src={props.imgsrc} alt="imageNetflix"/>
-        </div>
-        <div className="card-info">
-            <h2 className="title">{props.title} </h2>
-            <span className="description">{props.description}</span>
-            
-        </div>
-        <div className="btn">
-            <a href={props.link} className="play-btn">
-        </a>
-        </div>
-    </div>
-    
-   </>)
+import React from "react";
+import "./Cards.css";
+function Card(props) {
+	return (
+		<>
+			<div className="cards">
+				<div className="card-img">
+					<img src={props.imgsrc} alt="imageNetflix" />
+				</div>
+				<div className="card-detail">
+					<div className="card-info">
+						<h2 className="title">{props.title} </h2>
+						<p className="description">{props.description}</p>
+					</div>
+					<div className="button">
+						<a href={props.link} target="_blank">
+							{" "}
+							Click Here
+						</a>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default Card;
